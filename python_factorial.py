9ae4a27264efa224d2f0fc4a51ff8ff3b980a43d
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-12-02 23:27:33 +0530

    time for obtaining factorial using scipy function.

--- python_factorial.py
+++ python_factorial.py
@@ -1,20 +1,12 @@
+from scipy.special import factorial
+print("70!=",factorial(70),",")
+
 import timeit
 
-def factorial(k):
-    multiply=1;
-    for j in range(k):
-        multiply=multiply*(k-j);
-    return multiply
-print("70!=",factorial(70),".")
-
 code="""
-def factorial(k):
-    multiply=1;
-    for j in range(k):
-        multiply=multiply*(k-j);
-    return multiply
-    print(factorial(70))
+from scipy.special import factorial
+y=factorial(70)
 """
 
-t=timeit.timeit(code,number=1)
+t=timeit.timeit(code,number=1);
 print("time in seconds=",t,".")
