1111810f11163f445d233819d8faa21cfbca00ad
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-12-02 22:59:37 +0530

    creating arrays usin malloc.

---
+++ square_arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *main()                    
+{ 
+  int *y;
+  
+  y=malloc(100*sizeof(int));
+  
+  for (int j=0;j<100;j++) {y[j]=(j+1)*(j+1);};  /* creates an array of 1^2, 2^2, .......... */   
+ 
+  return (y);
+
+}
