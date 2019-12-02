943f602c9b038058cd0ce8062118d90e0d42f537
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-12-02 23:06:00 +0530

    printing array elements before sassignment.

--- square_arrays.c
+++ square_arrays.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float *mean_variance(int *p, int size);     /*take an array,return an array of mean and variance.*/
+
 int main()                    
 { 
   int *y;
-  y=calloc(100,sizeof(int));
+  y=malloc(100*sizeof(int));
   
   for (int j=0;j<100;j++)                   /* creates an array of 1^2, 2^2, .......... */
-    {	
-	y[j]=(j+1)*(j+1);
-	printf("%d\t",y[j]);	
-     };
+    {y[j]=(j+1)*(j+1);};
+
+  float *q;  				    /* q is an array of mean and  variance of any array.*/
+  q=mean_variance(y,100);
+  
+  FILE *fp;
+  fp=fopen("result.txt","w");
+  fprintf(fp,"mean = %f\t variance = %f.",q[0],q[1]);
+  
+  free(y);
+  free(q);
  
   return 0;
-  free(y);
 }
+
+
+float  *mean_variance(int *p, int size)     /*take an array,return an array of mean and variance.*/
+{   
+  int j;
+  float mean, variance;
+ 
+  int sum=0;  
+  for (j=0;j<size;j++)			    /* it adds all element of any given array.*/
+    {sum += p[j];};
+  
+  mean=sum/(float)size;			    /* mean is average of the sum.*/
+
+  int sq_sum=0;  
+  for (j=0;j<size;j++)			    /* it adds squares of deviation for each element.*/
+    {sq_sum += (p[j]-mean)*(p[j]-mean);};
+
+  variance=sq_sum/(float)size;	                    /* variance = average of sum of squares of deviation.*/
+  
+  
+  float *w;  				    /* w is output array containing mean and variance.*/
+  w=malloc(2*sizeof(float));
+  w[0]=mean;
+  w[1]=variance;
+  
+  return(w);
+  
+  free(w);
+}
+
