93da4c8e91d8b1db292d2586ade10b3126b5ee82
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-12-02 23:29:01 +0530

    time to obtain factorial using c codings.

--- c_factorial.c
+++ c_factorial.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 long int factorial(int k);
 
+
 int main()
 {
-  int j;
-  long int result;
+  clock_t begin, end;
 
+  begin=clock();
+
+  int j=8;
+  
+  long int result;
   result=factorial(j);
   printf("%d!= %ld.\n",j,result);
-    
+
+  end=clock();
+
+  printf("time in seconds= %f.\n",(end-begin)/CLOCKS_PER_SEC);
+  
   return(result);
 }
 	 
@@ -19,7 +29,6 @@ long int factorial(int k)
 {
   int j;
   long int multiply=1;
-  for (j==0;j<k;j++) {multiply *= (k-j);};
+  for (j==0;j<k;j++) {multiply=multiply*(k-j);};
   return(multiply);
 }
-
