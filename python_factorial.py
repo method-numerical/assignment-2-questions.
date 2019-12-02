3242135b5c0d13a0ba6315ab998529cc5e56b09f
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-12-02 23:24:57 +0530

    python file to calculate factorial using scipy function.

--- python_factorial.py
+++ python_factorial.py
@@ -1,6 +1,2 @@
-def factorial(k):
-    multiply=1;
-    for j in range(k):
-        multiply=multiply*(k-j);
-    return multiply
-print("70!=",factorial(70),".")
+from scipy.special import factorial
+print("70!=",factorial(70),",")
